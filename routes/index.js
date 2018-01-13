3f7c32ce0201758630d5f863f8e7598f058685fa
Dave Kearney <dave@somnipod.com>
2018-01-13 22:47:07 +0000

    Added models folder and moved index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+console.log("index!!!");
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function(req, res, next) {  res.render('index', require('../models/index.js').data  );    });
+
+
+
+
+
+
+
+/*
+router.get('/android',         function(req, res, next) {  res.render('layouts/android.hbs', require('../models/android.js').data);   });
+*/
+
 
 module.exports = router;
